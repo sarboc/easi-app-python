37df43e602e6357797aad38bc60d90ab582432ef
Sara Bocciardi <sjbocciardi@gmail.com>
2021-02-20 19:47:58 -0800

    Add accessibility request.

--- easi_api/systems/models.py
+++ easi_api/systems/models.py
@@ -6,3 +6,12 @@ class System(models.Model):
 
     def __str__(self):
         return self.name
+
+class AccessibilityRequest(models.Model):
+    name = models.CharField(max_length=100)
+    system = models.ForeignKey(
+        System, related_name="system", on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
