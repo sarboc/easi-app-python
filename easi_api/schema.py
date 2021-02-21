38122cfa3a285aa69f8aa66e5b51a586812d4870
Sara Bocciardi <sjbocciardi@gmail.com>
2021-02-20 20:53:03 -0800

    Add schema work for test dates.

--- easi_api/schema.py
+++ easi_api/schema.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene_django import DjangoObjectType
 
-from easi_api.systems.models import AccessibilityRequest, System
+from easi_api.systems.models import AccessibilityRequest, AccessibilityTestDate, System
 
 class SystemType(DjangoObjectType):
     class Meta:
@@ -11,16 +11,25 @@ class SystemType(DjangoObjectType):
 class AccessibilityRequestType(DjangoObjectType):
     class Meta:
         model = AccessibilityRequest
-        fields = ("id", "name", "system")
+        fields = ("id", "name")
+
+class AccessibilityTestDateType(DjangoObjectType):
+    class Meta:
+        model = AccessibilityTestDate
+        fields = ("id", "date", "score")
 
 class Query(graphene.ObjectType):
     all_systems = graphene.List(SystemType)
     all_accessibility_requests = graphene.List(AccessibilityRequestType)
+    all_test_dates = graphene.List(AccessibilityTestDateType)
 
     def resolve_all_systems(root, info):
         return System.objects.all()
-
-    def resolve_all_accessiblity_requests(root, info):
+    
+    def resolve_all_accessibility_requests(root, info):
         return AccessibilityRequest.objects.all()
+
+    def resolve_all_test_dates(root, info):
+        return AccessibilityTestDate.objects.all()
 
 schema = graphene.Schema(query=Query)
